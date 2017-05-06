0a926459556d15abfbba79d87dd67f6cc6e37bd5
Dmitry Vylegzhanin <diod@psax.ru>
2017-05-06 14:17:41 +0300

    add another idle func with settings

--- dimmer-main.py
+++ dimmer-main.py
@@ -49,7 +49,7 @@ def dimmer_set(channel, value):
 
 #    print '>> ' + data.encode('hex')
     ser.write(data)
-    time.sleep(0.0050)
+    time.sleep(0.0060)
 #    rep = ser.read(8)
 #    time.sleep(0.0050)
 #    print '<< ' + rep.encode('hex');
@@ -76,6 +76,35 @@ def trapezoid_level(t, dly, bright, t_pulse, t_ramp, t_decay):
             
             return level
 
+def pulse_dual_level(t, dly, bright, t_pulse, t_ramp, t_decay):
+            t_pulse = 4*t_ramp;
+            dly_off = dly-t_pulse;
+            dly_on  = t_pulse;
+
+            level = -3
+
+            #implement trapezoid pulse
+            if (t < dly_off):	#not in pulse
+              in_pulse = 0
+              level = 0
+
+            elif ((t > dly_off) and (t < dly)):	#in pulse on chan 1
+              in_pulse = 1
+              if (t < dly_off+t_ramp): #ramp up
+                  level = int( ( t - dly_off )/t_ramp * bright );
+              elif (t < dly_off+2*t_ramp): #ramp down
+                  level = int( ( dly_off + 2*t_ramp - t )/t_ramp * bright );
+              elif (t < dly_off+3*t_ramp): #ramp up
+                  level = int( ( t - dly_off - 2*t_ramp )/t_ramp * bright );
+              elif (t < dly_off+4*t_ramp): #ramp down
+                  level = int( ( dly_off + 4*t_ramp - t )/t_ramp * bright );
+#              elif (t > dly-t_decay):
+#                  level = int( (dly - t)/t_decay * bright );
+              else:
+                  level = 0
+            
+            return level
+
 
 #redis
 db = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -89,9 +118,15 @@ t_ramp=0.050
 t_decay=0.050
 
 #idle
-t_pulse_idle=0.500
-t_ramp_idle=0.100
-t_decay_idle=0.100
+t_pulse_idle=1.000
+t_ramp_idle=0.160
+t_decay_idle=0.300
+
+#delay_idle not used for pulse_dual
+
+#t_pulse_idle=3.000
+#t_ramp_idle=1.000
+#t_decay_idle=1.000
 
 cmdtime = 0.0054 #command length
 
@@ -100,7 +135,7 @@ t1=time.time(); #now
 t2=t1;
 t_idle = t1;
 
-bpm_idle = 30
+bpm_idle = 24.0
 
 level1 = 0
 level2 = 0
@@ -110,7 +145,7 @@ chan_a=16+10
 chan_b=10
 outlvl = -1
 
-valid = 10 #time when not changing value is valid
+valid = 5 #time when not changing value is valid
 
 while True:
         try:
@@ -130,21 +165,49 @@ while True:
               bpm2upd = 0
 
             try:              
-              bright1 = int(db.get('bright.1'))
+              bright1 = float(db.get('bright.1'))
             except:
-              bright1 = 64
+              bright1 = 255.0
               
             try:
-              bright2 = int(db.get('bright.2'))
+              bright2 = float(db.get('bright.2'))
             except:
-              bright2 = 64
+              bright2 = 255.0
 
             try:
-              bright_idle = int(db.get('bright.idle'))
+              brightA = float(db.get('bright.a'))
             except:
-              bright_idle = 64
+              brightA = 255.0
 
-            #go
+            try:
+              brightB = float(db.get('bright.b'))
+            except:
+              brightB = 255.0
+
+            try:
+              bpm_idle = float(db.get('pulse.idle'))
+            except:
+              bpm_idle = 20.0
+
+            if (bpm_idle<2.0):
+              bpm_idle = 2.0
+
+            try:
+              bright_idle = float(db.get('bright.idle'))
+            except:
+              bright_idle = 255.0
+
+            try:
+              mode_a = int(db.get('mode.a'))
+            except:
+              mode_a = 0
+
+            try:
+              mode_b = int(db.get('mode.b'))
+            except:
+              mode_b = 0
+
+            #go - hrm 1,2
             now = time.time()
             if (bpm1 > 0 and bpm1upd + valid >= now):
               dly1 = 60.0/bpm1
@@ -161,19 +224,47 @@ while True:
               dly2 = 60.0/85.0;
               level2 = 0            
             
-            #idle - no info
+            #idle chan
             dly_idle = 60.0/bpm_idle
+            chan_idle = pulse_dual_level(now-t_idle, dly_idle, bright_idle, t_pulse_idle, t_ramp_idle, t_decay_idle);
+
+            #idle if no hrm            
             if ( not (bpm2 > 0 and bpm2upd + valid >= now) and not (bpm1 > 0 and bpm1upd + valid >= now)):
-              dly_idle = 60.0/bpm_idle
-              level_idle = trapezoid_level(now-t_idle, dly_idle, bright_idle, t_pulse_idle, t_ramp_idle, t_decay_idle);  
+              level_idle = chan_idle
             else:
               level_idle = 0
 
+            #mix
+            chan_hrm_idle = max(level1,level2,level_idle); #max
+            chan_hrm = max(level1,level2);
+            
+            #channel modes
+            if (mode_a == 0):
+              chan_a_out = chan_hrm_idle;
+            elif (mode_a == 1):
+              chan_a_out = chan_idle;
+            elif (mode_a == 2):
+              chan_a_out = chan_hrm;
+            elif (mode_a == 3):
+              chan_a_out = 0;
+            elif (mode_a == 4):
+              chan_a_out = 255;
 
-#            print "bpm1: %d (%f), bpm2: %d (%f), level1: %d, level2: %d, level_idle: %d" % (bpm1,bpm1upd,bpm2,bpm2upd,level1,level2,level_idle)
+            if (mode_b == 0):
+              chan_b_out = chan_hrm_idle;
+            elif (mode_b == 1):
+              chan_b_out = chan_idle;
+            elif (mode_b == 2):
+              chan_b_out = chan_hrm;
+            elif (mode_b == 3):
+              chan_b_out = 0;
+            elif (mode_b == 4):
+              chan_b_out = 255;
+              
+#            print "ma: %d, mb: %d, ba: %d, bb: %d, bi: %d, bpm1: %d (%f), bpm2: %d (%f), h1o: %d, h2o: %d, idle_out: %f, a: %d, b:%d, ci:%d" % (mode_a,mode_b,brightA,brightB,bright_idle,bpm1,bpm1upd,bpm2,bpm2upd,level1,level2,level_idle,chan_a_out,chan_b_out,chan_idle)
 
             #mix func
-            lvl = max(level1,level2,level_idle); #max
+            #chan_hrm = max(level1,level2);
 
             if (now-t1 > dly1):
               t1 = t1+dly1
@@ -194,11 +285,11 @@ while True:
             if (now-t_idle > 100 or now < t_idle):
               t_idle = now
 
-            if outlvl != lvl: 
-              dimmer_set(chan_a, lvl);
-              dimmer_set(chan_b, lvl);
-              outlvl = lvl
-              continue
+#            if outlvl != lvl: 
+            dimmer_set(chan_a, int(chan_a_out*brightA/255.0));
+            dimmer_set(chan_b, int(chan_b_out*brightB/255.0));
+#            outlvl = lvl
+            continue
 
             # 2ms
             time.sleep(0.002)
